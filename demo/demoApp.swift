858db8e40059f5de17b233df228d6667ed41676a
hajarala <hajaralanzi24@gmail.com>
2024-09-26 17:00:10 +0300

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Hajar Alanazi on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
